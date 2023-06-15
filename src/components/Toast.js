263cf78d84b8274ff98c37d005723af09b92dd6d
parvejg <parvezpg321@gmail.com>
2023-06-16 01:45:05 +0530

    Toast carbon add

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,41 +1,51 @@
 import "./Toast.css";
 import { ImWarning } from "react-icons/im";
 import { BsInfoCircle } from "react-icons/bs";
-import {CgDanger} from "react-icons/cg"
-import {BiCheck} from "react-icons/bi"
-export const WarningToast = () => {
+import { CgDanger } from "react-icons/cg";
+import { BiCheck } from "react-icons/bi";
+export const WarningToast = (props) => {
+  const { warningToastIconWrapperClass, warningIconClass } = props;
   return (
     <div>
-      <p className="warning-toast-icon-wrapper">
-        <ImWarning className="warning-toast-icon" />
+      <p
+        className={`warning-toast-icon-wrapper ${warningToastIconWrapperClass}`}
+      >
+        <ImWarning className={`warning-toast-icon ${warningIconClass}`} />
       </p>
     </div>
   );
 };
-export const InfoToast = () => {
+export const InfoToast = (props) => {
+  const { infoToastIconWrapperClass, infoToastIconClass } = props;
   return (
     <div>
-      <p className="info-toast-icon-wrapper">
-        <BsInfoCircle className="info-toast-icon" />
+      <p className={`info-toast-icon-wrapper ${infoToastIconWrapperClass}`}>
+        <BsInfoCircle className={`info-toast-icon ${infoToastIconClass}`} />
       </p>
     </div>
   );
 };
-export const DangerToast = () => {
+export const DangerToast = (props) => {
+  const { dangerToastIconWrapperClass, dangerToastIconClass } = props;
   return (
     <div>
-      <p className="danger-toast-icon-wrapper">
-        <CgDanger className="danger-toast-icon" />
+      <p className={`danger-toast-icon-wrapper ${dangerToastIconWrapperClass}`}>
+        <CgDanger className={`danger-toast-icon ${dangerToastIconClass}`} />
       </p>
     </div>
   );
 };
-export const SuccessfulToast = () => {
-    return (
-      <div>
-        <p className="successful-toast-icon-wrapper">
-          <BiCheck className="successful-toast-icon" />
-        </p>
-      </div>
-    );
-  };
+export const SuccessfulToast = (props) => {
+  const { successfulToastIconWrapperClass, successfulToastIconClass } = props;
+  return (
+    <div>
+      <p
+        className={`successful-toast-icon-wrapper ${successfulToastIconWrapperClass}`}
+      >
+        <BiCheck
+          className={`successful-toast-icon ${successfulToastIconClass}`}
+        />
+      </p>
+    </div>
+  );
+};
