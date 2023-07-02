4eb63da7df2614b15fb750519595577e15bcdb31
parvejg <parvezpg321@gmail.com>
2023-07-02 13:37:47 +0530

    add links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import './Footer.css';
 export const Footer=(props)=>{
     const {href, name}= props;
 return   <div href={href} className="layout-footer-wrapper">
-<a href={href} className="layout-footer-links layout-footer-links">{name ? name: "LinkedIn"}</a>
-<a href={href} className="layout-footer-links layout-footer-links">{name ? name: "Github"}</a>
-<a href={href} className="layout-footer-links layout-footer-links">{name ? name: "Instagram"}</a>
+<a href="https://www.linkedin.com/in/md-parvez-6b6a5a259/" className="layout-footer-links layout-footer-links">{name ? name: "LinkedIn"}</a>
+<a href="https://github.com/parvejg/component-library-321" className="layout-footer-links layout-footer-links">{name ? name: "Github"}</a>
+<a href="https://instagram.com/parvez321pg" className="layout-footer-links layout-footer-links">{name ? name: "Instagram"}</a>
 </div>
 }
