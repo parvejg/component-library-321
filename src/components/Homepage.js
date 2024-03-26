7346bbc4f90c9f5b7b85cfc7dd3272cfe0926175
parvejg <parvezpg321@gmail.com>
2024-03-26 22:37:15 +0530

    add naviget on get start btn

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,24 @@
-import "./HomePage.css"
-import { LayoutNavbar } from "./Navbar"
-export const Homepage=()=>{
-    return <div>
-        <LayoutNavbar />
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+import { LayoutNavbar } from "./Navbar";
+export const Homepage = () => {
+  const naviget = useNavigate();
+  return (
+    <div>
+      <LayoutNavbar />
 
-        <div className="home-page-content-wrapper">
-            <h1>Welcome to my<span className="components-txt"> Components Library</span></h1>
-            <a href="avatar-page" className="home-page-links">Get Start</a>
-
-        </div>
+      <div className="home-page-content-wrapper">
+        <h1>
+          Welcome to my
+          <span className="components-txt"> Components Library</span>
+        </h1>
+        <button
+          onClick={() => naviget("/avatar-page")}
+          className="home-page-links"
+        >
+          Get Start
+        </button>
+      </div>
     </div>
-}
+  );
+};
